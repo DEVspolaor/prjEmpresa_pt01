014223645f04bfd6fc410b2ea55d72e34b78ac62
Spolaor <133894740+DEVspolaor@users.noreply.github.com>
2023-11-29 13:20:39 -0300

    Update Funcionario.java

--- src/main/java/com/senai/PrjEmpresa/entities/Funcionario.java
+++ src/main/java/com/senai/PrjEmpresa/entities/Funcionario.java
@@ -42,5 +42,34 @@ public class Funcionario {
 	@NotNull
 	@NotBlank
 	private Double Salario;
+
+
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public LocalDate getFunnascimento() {
+		return funnascimento;
+	}
+	public void setFunnascimento(LocalDate funnascimento) {
+		this.funnascimento = funnascimento;
+	}
+	public Double getSalario() {
+		return salario;
+	}
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+	private Double salario;
+	
 	
 }
